e0e5ef7605fd192f1458ca10242f1b15265308a5
Choonsik <101438447+moodeary@users.noreply.github.com>
2024-01-26 18:42:29 +0900

    backend connect

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
-    <h1 @click="saveToDo">This is an about pagee</h1>
+    <div style="display: flex; flex-direction: column">
+      <h1 @click="saveToDo">메세지 생성</h1>
+      <h1 @click="createMember">회원가입 버튼</h1>
+      <h1 @click="loginMember">로그인 버튼</h1>
+    </div>
   </div>
 </template>
 
@@ -13,12 +17,44 @@ export default {
       if (this.todo != '') {
         axios
           .post('/api/v1/todo/save', {
-            title: '야호'
+            title: '메세지 입력 해주세요.'
+          })
+          .then(() => {
+            console.log('ToDo 메세지 생성')
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      } else {
+        console.log('내용을 입력해주세요.')
+      }
+    }
+
+    function createMember() {
+      axios
+        .post('/api/v1/member/signUp', {
+          loginId: 'admin',
+          nickName: 'admin',
+          password: 'admin'
+        })
+        .then(() => {
+          console.log('회원가입')
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
+    function loginMember() {
+      if (this.todo != '') {
+        axios
+          .post('/api/v1/member/login', {
+            loginId: 'admin',
+            nickName: 'admin',
+            password: 'admin'
           })
           .then((res) => {
-            console.log('응답값')
-            // this.reviewList.push(res.data);
-            // this.todo = "";
+            console.log('로그인', res)
           })
           .catch((err) => {
             console.log(err)
@@ -29,7 +65,9 @@ export default {
     }
 
     return {
-      saveToDo
+      saveToDo,
+      createMember,
+      loginMember
     }
   }
 }
